85e21f6fe5b81fdc34bde1beebbbf1a8e48b02a8
Arthur Biasibetti Farias <arthurbiasibettifarias@gmail.com>
2023-06-14 20:19:23 -0300

    JWT funcionando

--- src/middlewares/authentication/index.ts
+++ src/middlewares/authentication/index.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import { get } from 'lodash';
 import { verifyJwt } from '../../utils/security';
-import { JwtPayload } from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
 
 export function expressAuthentication(
   request: express.Request,
@@ -9,22 +9,34 @@ export function expressAuthentication(
   scopes?: string[]
 ): any {
   if (securityName === 'jwt') {
-    const accessToken = get(request, 'headers.authorization', '').replace(
-      /^Bearer\s/,
+    const token = get(request, 'headers.authorization', '').replace(
+      /Bearer\s/,
       ''
     );
 
-    if (!accessToken && request.res) {
-      return request.res
-        .status(401)
-        .json({ message: 'User is not authenticated' });
-    }
-
-    const user = verifyJwt(accessToken);
-
-    if (user.decoded && request.res?.locals && request.next) {
-      request.res.locals = user.decoded as JwtPayload;
-      request.next();
-    }
+    return new Promise((resolve, reject) => {
+      console.log('TOKEN: ', token);
+      if (!token) {
+        reject(new Error('No token provided'));
+      }
+      jwt.verify(
+        token,
+        process.env.JWT_SECRETE || '',
+        function (err: any, decoded: any) {
+          if (err) {
+            reject(err);
+          } else {
+            if (scopes) {
+              for (const scope of scopes) {
+                if (!decoded.scopes.includes(scope)) {
+                  reject(new Error('JWT does not contain required scope.'));
+                }
+              }
+              resolve(decoded);
+            }
+          }
+        }
+      );
+    });
   }
 }
