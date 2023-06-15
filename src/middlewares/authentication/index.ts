71f9fa9cd409a07b724e3aa2466ba6f80d76cd5c
Arthurbiasibetti <arthurbiasibettifarias@gmail.com>
2023-06-14 21:17:47 -0300

    Mudanças do jwt

--- src/middlewares/authentication/index.ts
+++ src/middlewares/authentication/index.ts
@@ -1,42 +1,30 @@
 import * as express from 'express';
 import { get } from 'lodash';
-import { verifyJwt } from '../../utils/security';
 import * as jwt from 'jsonwebtoken';
+import { verifyJwt } from '../../utils/security';
+import { UnauthorizedError } from '../../Errors/instances/UnauthorizedError';
 
 export function expressAuthentication(
   request: express.Request,
   securityName: string,
   scopes?: string[]
-): any {
-  if (securityName === 'jwt') {
-    const token = get(request, 'headers.authorization', '').replace(
-      /Bearer\s/,
-      ''
-    );
+): Promise<any> {
+  const token = get(request, 'headers.authorization', '').replace(
+    /Bearer\s/,
+    ''
+  );
 
-    return new Promise((resolve, reject) => {
-      console.log('TOKEN: ', token);
-      if (!token) {
-        reject(new Error('No token provided'));
-      }
-      jwt.verify(
-        token,
-        process.env.JWT_SECRETE || '',
-        function (err: any, decoded: any) {
-          if (err) {
-            reject(err);
-          } else {
-            if (scopes) {
-              for (const scope of scopes) {
-                if (!decoded.scopes.includes(scope)) {
-                  reject(new Error('JWT does not contain required scope.'));
-                }
-              }
-              resolve(decoded);
-            }
-          }
-        }
-      );
-    });
-  }
+  return new Promise((resolve, reject) => {
+    if (!token) {
+      reject(new Error('No token provided'));
+    }
+
+    const jwtData = verifyJwt(token);
+
+    if (!jwtData.decoded) {
+      reject(new UnauthorizedError('JWT Invalido!'));
+    }
+
+    resolve(jwtData.decoded);
+  });
 }
